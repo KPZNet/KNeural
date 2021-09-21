b11d543f3f9c1a33bef6f93c463bbf01f9874b08
Kenneth Ceglia <KPZNet@outlook.com>
2021-09-21 09:21:21 -0700

    devs

--- KNeuralNet.py
+++ KNeuralNet.py
@@ -28,21 +28,20 @@ def plot_weights(epocs, weight_history) :
     plt.legend ()
     plt.show ()
 
-# input data
+
 inputs = np.array([[0, 1, 0],
                    [0, 1, 1],
                    [0, 0, 0],
                    [1, 0, 0],
                    [1, 1, 1],
                    [1, 0, 1]])
-# output data
+
 outputs = np.array([[0], [0], [0], [1], [1], [1]])
 
-# create NeuralNetwork class
 class NeuralNetwork:
 
     def __init__(self, inputs, outputs):
-        np.random.seed(123)
+        np.random.seed(99)
         self.inputs  = inputs
         self.outputs = outputs
         # initialize weights as normal random vars
@@ -50,9 +49,9 @@ class NeuralNetwork:
         self.error_history = []
         self.epoch_list = []
         self.weight_history = []
+        self.stop_delta = 0.095
 
-    #activation function ==> S(x) = 1/1+e^(-x)
-    def sigmoid(self, x, deriv=False):
+    def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_derivative(self, x):
@@ -67,15 +66,23 @@ class NeuralNetwork:
         self.weights += np.dot(self.inputs.T, delta)
         self.weight_history.append( copy.deepcopy(self.weights) )
 
-    def train(self, epochs=100):
+    def train(self, epochs=2500):
         for epoch in range(epochs):
-            # flow forward and produce an output
-            self.feed_forward()
-            # go back though the network to make corrections based on the output
-            self.backpropagation()    
-            # keep track of the error history over each epoch
-            self.error_history.append(np.average(np.abs(self.error)))
-            self.epoch_list.append(epoch)
+            stop = self.run_epoch(epoch)
+            if stop:
+                break
+
+    def run_epoch(self, epoch):
+        stop = False
+        self.feed_forward()
+        self.backpropagation()
+
+        err = np.average(np.abs(self.error))
+        if err < self.stop_delta:
+            stop = True
+        self.error_history.append(err)
+        self.epoch_list.append(epoch)
+        return stop
 
     # function to predict output on new and unseen input data                               
     def predict(self, new_input):
