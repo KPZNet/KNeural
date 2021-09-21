00f390960c2f845cbb4061896d655ef0f31394dc
Kenneth Ceglia <KPZNet@outlook.com>
2021-09-21 13:03:35 -0700

    devs

--- KNeuralNet.py
+++ KNeuralNet.py
@@ -85,7 +85,7 @@ class NeuralNetwork:
         self.weights += np.dot(self.inputs.T, delta)
         self.weight_history.append(copy.deepcopy(self.weights))
 
-    def train(self, epochs=2500):
+    def train(self, epochs=250):
         for epoch in range(epochs):
             stop = self.run_epoch(epoch)
             if stop:
@@ -103,24 +103,20 @@ class NeuralNetwork:
         self.epoch_list.append(epoch)
         return stop
 
-    # function to predict output on new and unseen input data                               
     def predict(self, sigmoid_fn, new_input):
         prediction = sigmoid_fn(np.dot(new_input, self.weights))
         return prediction
 
 
-# create neural network
 NN = NeuralNetwork(inputs, outputs)
-# train neural network
 NN.train()
 
-# create two new examples to predict                                   
-example = np.array([[1, 1, 0, 1]])
-example_2 = np.array([[0, 1, 1, 1]])
+run_test_1 = np.array([[1, 1, 0, 1]])
+run_test_2 = np.array([[0, 1, 1, 1]])
 
 # print the predictions for both examples                                   
-print(NN.predict(sigmoid_fn=sigmoidA, new_input=example), ' - Correct: ', 0)
-print(NN.predict(sigmoid_fn=sigmoidA, new_input=example_2), ' - Correct: ', 1)
+print(NN.predict(sigmoid_fn=sigmoidA, new_input=run_test_1), ' - Answer: ', 0)
+print(NN.predict(sigmoid_fn=sigmoidA, new_input=run_test_2), ' - Answer: ', 1)
 
 plot_error(NN.epoch_list, NN.error_history)
 plot_weights(NN.epoch_list, NN.weight_history)
