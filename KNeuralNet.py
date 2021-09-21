b8ad264015a3cb5270080e24e0dd52cdf46a997d
Kenneth Ceglia <KPZNet@outlook.com>
2021-09-21 08:57:53 -0700

    devs

--- KNeuralNet.py
+++ KNeuralNet.py
@@ -1,5 +1,32 @@
 import numpy as np # helps with the math
 import matplotlib.pyplot as plt # to plot error during training
+import copy
+
+
+def plot_error(epocs, error_history) :
+
+    plt.plot ( epocs, error_history, label="errors" )
+
+    plt.title ( "Error Rates" )
+    plt.xlabel ( "EPOCs" )
+    plt.ylabel ( "error" )
+    plt.legend ()
+    plt.show ()
+
+def plot_weights(epocs, weight_history) :
+    wh = np.array ( weight_history )
+    w_0 = wh[:, 0]
+    w_1 = wh[:, 1]
+    w_2 = wh[:, 2]
+    plt.plot ( epocs, w_0, label="weight 0" )
+    plt.plot ( epocs, w_1, label="weight 1" )
+    plt.plot ( epocs, w_2, label="weight 2" )
+
+    plt.title ( "Weights" )
+    plt.xlabel ( "EPOCs" )
+    plt.ylabel ( "weight" )
+    plt.legend ()
+    plt.show ()
 
 # input data
 inputs = np.array([[0, 1, 0],
@@ -14,33 +41,32 @@ outputs = np.array([[0], [0], [0], [1], [1], [1]])
 # create NeuralNetwork class
 class NeuralNetwork:
 
-    # intialize variables in class
     def __init__(self, inputs, outputs):
         self.inputs  = inputs
         self.outputs = outputs
-        # initialize weights as .50 for simplicity
-        self.weights = np.array([[.50], [.50], [.50]])
+        # initialize weights as normal random vars
+        self.weights = np.array([[np.random.normal()], [np.random.normal()], [np.random.normal()]])
         self.error_history = []
         self.epoch_list = []
+        self.weight_history = []
 
     #activation function ==> S(x) = 1/1+e^(-x)
     def sigmoid(self, x, deriv=False):
-        if deriv == True:
-            return x * (1 - x)
         return 1 / (1 + np.exp(-x))
 
-    # data will flow through the neural network.
+    def sigmoid_derivative(self, x):
+        return x * (1 - x)
+
     def feed_forward(self):
         self.hidden = self.sigmoid(np.dot(self.inputs, self.weights))
 
-    # going backwards through the network to update weights
     def backpropagation(self):
         self.error  = self.outputs - self.hidden
-        delta = self.error * self.sigmoid(self.hidden, deriv=True)
+        delta = self.error * self.sigmoid_derivative(self.hidden)
         self.weights += np.dot(self.inputs.T, delta)
+        self.weight_history.append( copy.deepcopy(self.weights) )
 
-    # train the neural net for 25,000 iterations
-    def train(self, epochs=25000):
+    def train(self, epochs=100):
         for epoch in range(epochs):
             # flow forward and produce an output
             self.feed_forward()
@@ -68,9 +94,6 @@ example_2 = np.array([[0, 1, 1]])
 print(NN.predict(example), ' - Correct: ', example[0][0])
 print(NN.predict(example_2), ' - Correct: ', example_2[0][0])
 
-# plot the error over the entire training duration
-plt.figure(figsize=(15,5))
-plt.plot(NN.epoch_list, NN.error_history)
-plt.xlabel('Epoch')
-plt.ylabel('Error')
-plt.show()
+
+plot_error(NN.epoch_list, NN.error_history)
+plot_weights(NN.epoch_list, NN.weight_history)
