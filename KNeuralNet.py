ac09ffc0ce19d9a4238b311ff19bb89f2b3479d2
KPZNET <kpznet@outlook.com>
2021-09-23 14:27:06 -0700

    dev

--- KNeuralNet.py
+++ KNeuralNet.py
@@ -75,8 +75,8 @@ class NeuralNetwork:
         return prediction
 
 def test_net(nnet):
-    run_test_1 = np.array([[1, 0, 1, 0, 0, 0]])
-    run_test_2 = np.array([[1, 1, 0, 1, 1, 0]])
+    run_test_1 = np.array([[1, 1, 1, 0, 0, 0]])
+    run_test_2 = np.array([[1, 0, 0, 1, 1, 0]])
     print(nnet.predict(sigmoid_fn=sigmoidA, new_input=run_test_1), ' - Answer: ', 1)
     print(nnet.predict(sigmoid_fn=sigmoidA, new_input=run_test_2), ' - Answer: ', 0)
 
@@ -84,8 +84,6 @@ np.random.seed(6)
 row, col = 50, 6
 
 inputsA = np.random.randint(2, size=(row,col))
-for n in inputsA:
-    n[0] = n[col-1] = n[1]
 outputsA = np.array([ inputsA[:, 1] ]).T
 
 NNN = NeuralNetwork()
